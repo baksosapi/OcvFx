08beb10b461c59b31425843b9482007100ac56f9
wildan.mbp <wildan.ubg@gmail.com>
2017-08-04 04:56:37 -0400

    add facerecognizer library

--- src/com/company/FaceDetector.java
+++ src/com/company/FaceDetector.java
@@ -8,6 +8,7 @@ import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -22,9 +23,11 @@ public class FaceDetector {
     static FaceDetector fd;
     private static String photoName;
     private static CascadeClassifier faceDetector;
+    private static CascadeClassifier eyesDetector;
     private static String photoPath;
     private static LogReport logReport;
     private static int allZeroFace = 0;
+    private static int allZeroEyes = 0;
     ClassLoader classLoader = getClass().getClassLoader();
     static File inFile;
     static List<String> list = new ArrayList<>();
@@ -38,12 +41,20 @@ public class FaceDetector {
 
         fd = new FaceDetector();
 
-        String cFile = fd.getFilePath("haarcascade_frontalface_alt.xml");
+        String detFaces = fd.getFilePath("haarcascade_frontalface_alt.xml");
+        String detEyes = fd.getFilePath("haarcascade_eye.xml");
 //        photoPath =  fd.getFilePath("./image");
 //        photoPath =  fd.getFilePath("./image/lena.png");
 //        photoPath =  fd.getFilePath("./imageTest");
-        photoPath =  fd.getFilePath("./imageTestOSorigin");
+//        photoPath =  fd.getFilePath("./imageTestOSorigin");
 //        photoPath =  fd.getFilePath("./imageTestOS");
+//        photoPath =  fd.getFilePath("./imageTestOSorigin/15000168.jpg"); // 709
+//        photoPath =  fd.getFilePath("./imageTestOSorigin/97003104.jpg"); // 709
+        photoPath =  fd.getFilePath("./imageTestOSorigin/93005077.jpg"); // 112
+//        photoPath =  fd.getFilePath("./imageTestOSorigin/97003519.jpg"); // 112
+//        photoPath =  fd.getFilePath("./imageTestOSorigin/11001806.jpg");
+//        photoPath =  fd.getFilePath("./imageTestOSorigin/12000190.jpg");
+//        photoPath =  fd.getFilePath("./imageTestOSorigin/13000938.jpg");
 //        photoPath =  fd.getFilePath("./imageTestOS/93001571.jpg");
 //        photoPath =  fd.getFilePath("./imageTest/10000099.jpg"); // 2
 //        photoPath =  fd.getFilePath("./imageTest/15000168.jpg"); // 8
@@ -52,18 +63,29 @@ public class FaceDetector {
 
         inFile = new File(photoPath);
 //        System.out.println("Is Directory : "+inFile.isDirectory());
-        faceDetector = new CascadeClassifier(cFile);
 
-        processFile(photoPath);
+        faceDetector = new CascadeClassifier(detFaces);
+        eyesDetector = new CascadeClassifier(detEyes);
 
-        System.out.println("Undetected Face = "+allZeroFace);
-//        processListFile();
+//        processFile(photoPath);
 
+        // Get List file from Text
+        // List of Files from "txt"
+        processListFile();
+
+        System.out.println("======================================");
+        System.out.println("Final Undetected Face = "+ allZeroFace);
+        System.out.println("Final Undetected Eyes = "+ allZeroEyes);
 
     }
 
     private static void processListFile() {
-        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileList.txt")))){
+//        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileList.txt")))){
+//        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileList23.txt")))){
+//        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileList800.txt")))){
+        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileList5.txt")))){
+//        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileUndetected.txt")))){
+//        try (Stream<String> stream = Files.lines(Paths.get(fd.getFilePath("fileFaceSize.txt")))){
 
             list = stream.collect(Collectors.toList());
 
@@ -71,10 +93,15 @@ public class FaceDetector {
             e.printStackTrace();
         }
 
+
         int i = 0;
         for (String path : list) {
-            processImage(fd.getFilePath("./imageTest/" + path), 0, 0, 0);
-            System.out.println("Process "+ i++ +"/"+list.size());
+            i++;
+//            System.out.println(path);
+            processImage(fd.getFilePath("./imageTestOSorigin/" + path), 0, 0, 0);
+
+            System.out.println("Process "+ i +"/"+ list.size());
+//            System.out.println("Undetected Face = "+ allZeroFace);
         }
 
     }
@@ -101,11 +128,18 @@ public class FaceDetector {
 //                    double scaleImage = j*0.1;
 //                    processImage(f.toString(), scaleImage, 0, 0 );
 
-                    processImage(f.toString(), 1, 0, 0 );
+                    processImage(f.toString(), 0, 0, 0 );
+                    System.out.println("Process : "+ i +"/"+ fileFolder.length);
+                    System.out.println("Undetected Face = "+ allZeroFace);
 
 //                System.out.println("Process : "+f.getName() +" - "+ Integer.toString(i) +" of "+fileFolder.length);
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
                 }
-                System.out.println("Undetected Face = "+allZeroFace);
+                System.out.println("Undetected Face = "+ allZeroFace);
 
 //            }
 
@@ -119,7 +153,12 @@ public class FaceDetector {
         Mat image = Highgui.imread( fName, Imgproc.COLOR_RGB2GRAY );
 
         MatOfRect faceDetections = new MatOfRect();
-        faceDetector.detectMultiScale(image, faceDetections, 1.05, 3, 0, new Size(), new Size());
+        MatOfRect eyesDetections = new MatOfRect();
+
+//        System.out.println(fName);
+
+        faceDetector.detectMultiScale(image, faceDetections, 1.05, 3, 0, new Size(116, 116), new Size());
+        eyesDetector.detectMultiScale(image, eyesDetections);
 
         //
         // https://stackoverflow.com/questions/20801015/recommended-values-for-opencv-detectmultiscale-parameters
@@ -135,16 +174,23 @@ public class FaceDetector {
 //        faceDetector.detectMultiScale(image, faceDetections, 1.1, 3, 2, new Size(50, 50), new Size());
 
         int numFace = faceDetections.toArray().length;
-        String[] faceSizes = new String[]{};
+        int numEyes = eyesDetections.toArray().length;
+
+        String[] faceSizes, eyeSizes;
         String[] faceBuff = new String[0];
+        String[] eyesBuff = new String[0];
+
         //        System.out.println(String.format("Detected %s faces", numFace ));
         if (numFace > 0) {
             for (int i = 0; i < numFace; i++){
                 faceSizes = new String[numFace];
-                System.out.println(numFace);
-                System.out.println(faceDetections.toArray()[i].size().toString());
+//                System.out.println(numFace);
+//                System.out.println(faceDetections.toArray()[i].size().toString());
+//                faceSizes[i] = Double.toString(faceDetections.toArray()[i].size().width);
                 faceSizes[i] = faceDetections.toArray()[i].size().toString();
                 faceBuff = ArrayUtils.addAll(faceBuff, faceSizes[i]);
+
+//                Array facePoints[i] = faceDetections.
 
 //                String[] both = ArrayUtils.addAll(faceSizes[i], faceDetections.toArray()[i].size().toString());
 //
@@ -164,34 +210,66 @@ public class FaceDetector {
 //        logReport.append(fName, numFace, faceDetections.size().toString(), faceSize.toString() );
         logReport.append(fName, numFace, Arrays.toString(faceBuff));
 //        System.out.println("Face Size: "+faceSize.toString());
+        System.out.println("Face Size: "+ Arrays.toString(faceBuff));
 
 //        System.out.println(Arrays.toString(faceDetections.toArray()));
 
+        MatOfRect[] det = new MatOfRect[2];
+        det[0] = faceDetections;
+        det[1] = eyesDetections;
 //        saveOutputImage(fName, image, faceDetections);
+        saveOutputImage(fName, image, det);
+
     }
 
-    private static void saveOutputImage(String fName, Mat image, MatOfRect faceDetections) {
+//    private static void saveOutputImage(String fName, Mat image, MatOfRect faceDetections) {
+    private static void saveOutputImage(String fName, Mat image, MatOfRect[] mor) {
 
         // Create rectangle to existing face
-        for (Rect rect : faceDetections.toArray()) {
+
+        ArrayList<Integer> faceList =  new ArrayList<>();
+        for (Rect rect : mor[0].toArray()) {
+
+            faceList.add(rect.width);
+
+            System.out.println(fName+" - "+ rect.x);
+
             Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
                     new Scalar(0, 255, 0));
         }
 
+        for (Rect rect : mor[1].toArray()) {
+            Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(255, 0, 0), 2);
+            Core.putText(image, "Eye", new Point(rect.x, rect.y - 5), 1, 2, new Scalar(0, 0, 255));
+
+        }
+
+//        Imgproc.putText(image, "Eye", new Point(rect.x,rect.y-5), 1, 2, new Scalar(0,0,255));
+//        Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+//                new Scalar(200, 200, 100),2);
+
+
         // Save Output OriImage+Rectangle
 //        File outDir = new File("");
         String filename = fName+"_o.png";
-        System.out.println(String.format("Writing %s", filename));
+//        System.out.println(String.format("Writing %s", filename));
         Highgui.imwrite(filename, image);
 
     }
 
     private String getFilePath(String fileName) {
 
-//        System.out.println(fileName);
+//        System.out.println(classLoader.getResource(fileName));
 
         File file = new File(classLoader.getResource(fileName).getFile());
 
         return file.getPath();
+    }
+
+    private void circlePoi(){
+
+        // Get list face
+        // Get list eyes
     }
 }
